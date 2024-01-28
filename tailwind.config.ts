ea8cc3d3f7d60abca4eb266b97a7cd1c28e95791
Oluwadabira <adigunsamuelaso@gmail.com>
2024-01-28 15:43:11 +0100

    Add custom colors and drop shadow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				lightCyan: 'hsl(193, 38%, 86%)',
+				neonGreen: 'hsl(150, 100%, 66%)',
+				grayishBlue: 'hsl(217, 19%, 38%)',
+				darkgrayishblue: 'hsl(217, 19%, 24%)',
+				darkblue: 'hsl(218, 23%, 16%)',
+			},
+			dropShadow: {
+				glow: [
+					'0 0px 15px hsl(150, 100%, 66%)',
+					'0 0px 25px hsla(150, 100%, 66%, 0.2)',
+				],
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
